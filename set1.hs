4e688c6ea3df5fcd5d299a1b5f130d3b845b63a4
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-03 22:11:47 +0100

    refactors `getRands` to run arbitrary generators, and solves problem 2.

--- set1.hs
+++ set1.hs
@@ -6,9 +6,15 @@ module Set1 where
 import MCPrelude
 
 fiveRands :: [Integer]
-fiveRands = getRands (mkSeed 1) 5
+fiveRands = getRands rand (mkSeed 1) 5
 
-getRands :: Seed -> Integer -> [Integer]
-getRands s n
+getRands :: (Seed -> (a, Seed)) -> Seed -> Integer -> [a]
+getRands gen s n
     | n <= 0 = []
-    | otherwise = let (r, ns) = rand s in r : getRands ns (n - 1)
+    | otherwise = let (r, ns) = gen s in r : getRands gen ns (n - 1)
+
+randLetter :: Seed -> (Char, Seed)
+randLetter s = let (r, ns) = rand s in (toLetter r, ns)
+
+randString3 :: String
+randString3 = getRands randLetter (mkSeed 1) 3
