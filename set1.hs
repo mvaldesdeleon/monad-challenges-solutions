fd66f662c03057ecf0ebf87748ae81d927d3d0da
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-03 22:30:20 +0100

    Abstracts mapping generators.

--- set1.hs
+++ set1.hs
@@ -15,8 +15,20 @@ getRands gen s n
     | n <= 0 = []
     | otherwise = let (r, ns) = gen s in r : getRands gen ns (n - 1)
 
+mapRand :: (a -> b) -> Gen a -> Gen b
+mapRand f ra s = let (a, ns) = ra s in (f a, ns)
+
 randLetter :: Gen Char
-randLetter s = let (r, ns) = rand s in (toLetter r, ns)
+randLetter = mapRand toLetter rand
 
 randString3 :: String
 randString3 = getRands randLetter (mkSeed 1) 3
+
+randEven :: Gen Integer -- the output of rand * 2
+randEven = mapRand (*2) rand
+
+randOdd :: Gen Integer -- the output of randEven + 1
+randOdd = mapRand (+1) randEven
+
+randTen :: Gen Integer -- the output of rand * 10
+randTen = mapRand (*10) rand
