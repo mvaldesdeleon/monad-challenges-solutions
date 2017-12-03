4ba0b2d9e5a33f23c75b9d4dffd710051e85d420
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-03 22:17:46 +0100

    Adds type synonym.

--- set1.hs
+++ set1.hs
@@ -5,15 +5,17 @@ module Set1 where
 
 import MCPrelude
 
+type Gen a = Seed -> (a, Seed)
+
 fiveRands :: [Integer]
 fiveRands = getRands rand (mkSeed 1) 5
 
-getRands :: (Seed -> (a, Seed)) -> Seed -> Integer -> [a]
+getRands :: Gen a -> Seed -> Integer -> [a]
 getRands gen s n
     | n <= 0 = []
     | otherwise = let (r, ns) = gen s in r : getRands gen ns (n - 1)
 
-randLetter :: Seed -> (Char, Seed)
+randLetter :: Gen Char
 randLetter s = let (r, ns) = rand s in (toLetter r, ns)
 
 randString3 :: String
