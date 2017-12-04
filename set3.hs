c51e14cceab4bed1fb83370a3ba31213d675ff17
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-04 17:52:41 +0100

    Set 3 part 5.

--- set3.hs
+++ set3.hs
@@ -17,12 +17,10 @@ allCards :: [Int] -> [String] -> [Card]
 allCards = liftList2 Card
 
 liftList2 :: (a -> b -> c) -> [a] -> [b] -> [c]
-liftList2 f as bs = concat $ map (pairsWith bs) as
-    where pairsWith bs a = map (f a) bs
+liftList2 f as bs = f `map` as `apList` bs
 
 liftList3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
-liftList3 f as bs cs = concat $ concat $ map (\a ->
-    map (\b ->
-        map (\c ->
-            f a b c
-        ) cs) bs) as
+liftList3 f as bs cs = f `map` as `apList` bs `apList` cs
+
+apList :: [a -> b] -> [a] -> [b]
+apList fs as = concat $ map (\a -> map ($ a) fs) as
