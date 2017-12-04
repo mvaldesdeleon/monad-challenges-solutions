757d9ae311b87d7ea8dbc63b0e27439e146dc4da
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-04 11:34:06 +0100

    set 2 part 2.

--- set2.hs
+++ set2.hs
@@ -11,3 +11,28 @@ data Maybe a = Nothing
 instance Show a => Show (Maybe a) where
     show Nothing = "Nothing"
     show (Just a) = "Just " ++ show a
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:xs) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (x:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay a [] = Nothing
+lookupMay a (p:ps) = if fst p == a then Just $ snd p
+                                   else lookupMay a ps
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay a b = if b == 0 then Nothing
+                       else Just (a / b)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay (x:xs) = Just $ foldl max x xs
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay (x:xs) = Just $ foldl min x xs
