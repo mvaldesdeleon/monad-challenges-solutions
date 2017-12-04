11a923173132be11f776ec08398d3c22ed972697
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-04 16:28:58 +0100

    Set 3 part 4.

--- set3.hs
+++ set3.hs
@@ -19,3 +19,10 @@ allCards = liftList2 Card
 liftList2 :: (a -> b -> c) -> [a] -> [b] -> [c]
 liftList2 f as bs = concat $ map (pairsWith bs) as
     where pairsWith bs a = map (f a) bs
+
+liftList3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+liftList3 f as bs cs = concat $ concat $ map (\a ->
+    map (\b ->
+        map (\c ->
+            f a b c
+        ) cs) bs) as
