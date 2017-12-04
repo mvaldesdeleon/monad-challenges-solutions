5f16f31d548a27f9425fb4124838d300c00dcae1
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-12-04 16:07:39 +0100

    Set 3 part 2.

--- set3.hs
+++ set3.hs
@@ -5,6 +5,14 @@ module Set3 where
 
 import MCPrelude
 
+data Card = Card Int String
+
+instance Show Card where
+    show (Card r s) = show r ++ s
+
 allPairs :: [a] -> [b] -> [(a,b)]
 allPairs as bs = concat $ map (pairsWith bs) as
     where pairsWith bs a = map ((,) a) bs
+
+allCards :: [Int] -> [String] -> [Card]
+allCards as bs = map (uncurry Card) $ allPairs as bs
